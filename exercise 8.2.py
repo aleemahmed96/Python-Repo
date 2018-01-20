a7a8075519a81a15113d4fcea1e20bfc817fae43
danidavid <dudeboy.aleemahmed@gmail.com>
2018-01-20 23:03:49 +0500

    working...

---
+++ exercise 8.2.py
@@ -0,0 +1,5 @@
+def favourite_book(title):
+    """[making a program to print favourite book]
+    """
+    print('One of my favoutite book is '+ title.title())
+favourite_book("'python crash course'")
