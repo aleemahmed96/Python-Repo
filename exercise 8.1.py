6e963d75190e56c7effce319194fb0ef76a2df1f
danidavid <dudeboy.aleemahmed@gmail.com>
2018-01-20 22:56:53 +0500

    Having fever but still working up.....

---
+++ exercise 8.1.py
@@ -0,0 +1,6 @@
+def display_message(name):
+    """[making a program to tell everyone that I am learning through this chapter]
+    
+    """
+    print('Hi everyone, My name is ' + name.title() + ' and I am learning this chapter')
+display_message('david')
