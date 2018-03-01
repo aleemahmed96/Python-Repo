2ac3052464ebff0388eafcdc549bd35836bcd48a
danidavid <dudeboy.aleemahmed@gmail.com>
2018-03-01 21:26:19 +0500

    working....

---
+++ exercise 8.7.py
@@ -0,0 +1,20 @@
+def make_album(artist_name,album_title):
+    """generating dictionary that describes about the music album"""
+
+    dictionary={"Artist name":artist_name,"Album title":album_title }
+    return dictionary
+
+
+count =0
+for count in range(3):
+    print("enter artist name")
+    artist_name=input("artist name: ")
+    print("enter album title")
+    album_title=input("album title: ")
+    signal = make_album(artist_name,album_title)
+    print(signal)
+
+ 
+
+    
+
