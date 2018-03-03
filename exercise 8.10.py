2c72708a085a4f4f082fa820c49048344dd90884
danidavid <dudeboy.aleemahmed@gmail.com>
2018-03-03 17:45:13 +0500

    working...

---
+++ exercise 8.10.py
@@ -0,0 +1,18 @@
+magician_list = ["chris","amil rustam bangali :D","bagali baba jharoo wale :D ","chote ustaad :P"]
+
+def show_magician_name(names):
+    """printing name of each magician in the list"""
+
+    print('The magician names are:\n')
+    for name in names:
+        print(name.title())
+
+
+def make_great(name):
+    """putting 'the great' in list name"""
+    
+    for name1 in name:
+        print("The Great",name1.title())
+
+make_great(magician_list)
+
