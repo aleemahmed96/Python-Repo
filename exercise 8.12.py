2123761665e7fa46edc5e008d4c227032c2842a8
danidavid <dudeboy.aleemahmed@gmail.com>
2018-03-05 03:45:42 +0500

    updated*

---
+++ exercise 8.12.py
@@ -0,0 +1,12 @@
+def toppings_name(*list1):
+    print("your requested topping\'s sandwich are: ")
+    for list2 in list1:
+        print(list2)
+
+toppings_name('salad','cheese','olive')
+toppings_name('mayo garlic','chicken spread','chatter sauce')
+toppings_name('x','y','z')
+
+
+
+     
