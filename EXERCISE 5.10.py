a50e402a2456bb634c9485f70605ff188089bbb4
danidavid <dudeboy.aleemahmed@gmail.com>
2018-03-07 14:58:51 +0500

    updated*

--- EXERCISE 5.10.py
+++ EXERCISE 5.10.py
@@ -2,7 +2,7 @@ CURRENT_USER = ['sadiq', 'sheeraz', 'noman', 'Kashif', 'ovais']
 NEW_USER = ['Ovais', 'KASHIF', 'ahmed', 'faraz', 'rehan']
 
 for new in NEW_USER:
-    if new.capitalize() in CURRENT_USER or new.lower() in CURRENT_USER:
+    if new.capitalize() in CURRENT_USER or new.lower() in CURRENT_USER or new.upper() in CURRENT_USER:
         print('please change your name')
     else:
         print('Welcome')
--- example 8.3.py
+++ example 8.3.py
@@ -1,6 +1,10 @@
+#Write a function called make_shirt() that accepts a size and the
+#text of a message that should be printed on the shirt. The function should print
+#a sentence summarizing the size of the shirt and the message printed on it.
+#Call the function once using positional arguments to make a shirt. Call the
+#function a second time using keyword arguments.
 
 def make_shirt(size,text):
-
 
     """[making a function to show the size and text that should be printed on shirt]
     
@@ -8,10 +12,7 @@ def make_shirt(size,text):
         size {string} -- [XS,S,M,L,XL]
     """
 
-
     print("This shirt is " + size + "." + text)
-
-
 
 #when using by positional arguments    
 make_shirt("XS"," Happy Valentines Day")
