6fd717f983c8ec993cfe9dab6428ea38a22738dd
danidavid <dudeboy.aleemahmed@gmail.com>
2018-03-08 03:28:55 +0500

    now working on exercises in chapter 9

---
+++ exercise 9.1.py
@@ -0,0 +1,26 @@
+class Restaurant():
+    """making class restaurant that defines restaurant name and cuisine type
+    """
+    def __init__(self,name,cuisine):
+        """defining built-in method of python  (formerly called 'functions' before classes introduction) 
+        
+        Arguments:
+            name {string} -- name of restaurant
+            cuisine {string} -- type of food(cuisine)
+        """
+        self.restaurant_name = name
+        self.cuisine_type = cuisine
+    
+    def describe_restaurant(self):
+        print("This is " + self.restaurant_name.title() + " restaurant.")
+        print("This restaurant has " + self.cuisine_type.title() + " cuisine.")
+    
+    def open_restaurant(self):
+        print(self.restaurant_name.title() + " Restaurant is open.")
+
+restaurant_obj = Restaurant('Chinatown','Chinese')
+restaurant_obj.__init__
+restaurant_obj.describe_restaurant()
+restaurant_obj.open_restaurant()
+
+    
