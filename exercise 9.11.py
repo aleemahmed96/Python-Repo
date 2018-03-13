58b72736adafc487e00b7e0e8ae39c7aeeee9a79
danidavid <dudeboy.aleemahmed@gmail.com>
2018-03-14 01:29:03 +0500

    working... see file user_admn.py for this exercise

---
+++ exercise 9.11.py
@@ -0,0 +1,8 @@
+"""Making an Import from a Module named 'user_admin'. 
+    See user_admin.py for this exercise.
+"""
+
+import user_admin
+
+adminx = user_admin.Administrator()
+adminx.px.show_privileges()
---
+++ user_admin.py
@@ -0,0 +1,55 @@
+class User():
+    """docstring for User."""
+    def __init__(self, f_name, l_name, gender, age, joined):
+        self.firstname = f_name
+        self.lastname = l_name
+        self.age = age
+        self.gender = gender
+        self.joined = joined
+        self.login_attempts = 0
+
+    def describe_user(self):
+        print("\nContact Details")
+        print('Name: ' + self.firstname + " " + self.lastname)
+        print("Age: " + self.age)
+        print("Gender: " + self.gender)
+        print("Joined: " + self.joined)
+
+    def greet_user(self):
+        print("\nHello " + self.firstname + "!")
+
+    def increment_login_attempts(self):
+        self.login_attempts += 1
+        print("You have attempted " + str(self.login_attempts) + " login attempts.")
+
+    def reset_login_attempts(self):
+        self.login_attempts = 0
+        print("The Login attempt has been reset to " + str(self.login_attempts) + ".")
+
+#obj = User("dani","david","Male","21","25 feb 2018")
+#obj.increment_login_attempts()
+#obj.increment_login_attempts()
+#obj.increment_login_attempts()
+#obj.reset_login_attempts()
+
+class Privileges():
+    def __init__(self,privileges=["can add post","can delete post","can ban user"]):
+        self.priv = privileges
+
+    def show_privileges(self):
+            """used to show privileges
+            """
+            for self.p in self.priv:
+                msg = "\n The admin has privileges: \n"
+                msg += str(self.p)
+                print(msg)
+
+class Administrator(User):
+    """making a class for exercise 9.7 from exercise 9.5
+    """
+    def __init__(self):
+        self.px = Privileges()
+
+
+#admin = Administrator()
+#admin.px.show_privileges()
