8e24402069870b7ee15f17398b4c1026e6964596
Iwan <34235089+iwies@users.noreply.github.com>
2018-03-13 22:47:35 +0200

    Update exercise 5.6.py
    Fix typos

--- exercise 5.6.py
+++ exercise 5.6.py
@@ -2,14 +2,14 @@ age=int(input('enter your age'))
 if age>=0 and age<2:
     print ("this is a baby")
 elif age>=2 and age<4:
-    print ("this is a toodler")
+    print ("this is a toddler")
 elif age>=4 and age<13:
     print ("this is a kid")
 elif age>=13 and age<20:
     print ("this is a teenager")
 elif age>=20 and age<65:
-    print ("this is a adult")
+    print ("this is an adult")
 elif age>=65:
-    print ("this is a elder")
+    print ("this is an elder")
 else:
     print ('you have lost your mind!!!')
