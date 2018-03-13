d4d6d03435fc4ea8e37ec3a0fc9df865b78a8d68
danidavid <dudeboy.aleemahmed@gmail.com>
2018-03-14 00:13:14 +0500

    working.. (see restaurant.py for exercise 9.10)

---
+++ exercise 9.10.py
@@ -0,0 +1,10 @@
+"""Making an Import from a Module named 'restaurant' 
+"""
+
+from restaurant import Restaurant
+
+#see "restaurant.py" for this exercise
+objectx = Restaurant("chasmish","desi")
+objectx.number_served = 23
+objectx.increment_number_served(26)
+objectx.set_number_served(25) 
---
+++ restaurant.py
@@ -0,0 +1,44 @@
+class Restaurant():
+    """making class restaurant that defines restaurant name and cuisine type
+    """
+    def __init__(self,name,cuisine):
+        """ making method fot exercise 9.4 """
+        self.restaurant_name = name
+        self.cuisine_type = cuisine
+        self.number_served = 0
+    
+    def set_number_served(self, set1):
+        """ changing value of customers served by using method """
+        self.number_served = set1
+        print("Number of customers served: " + str(self.number_served))
+    
+    def increment_number_served(self, inc):
+        self.number_served += inc
+        print("The maximum number of customers served are: " + str(self.number_served))
+
+#restaurant = Restaurant("xyz","abc")
+#restaurant.number_served = 23
+#print(restaurant.number_served)
+
+#restaurant.set_number_served(25)
+#restaurant.increment_number_served(28)
+
+class IceCreamStand(Restaurant):
+    """ Making class for exercise 9.6 from exercise 9.4
+    """
+    def __init__(self,flavors=['choco','strawberry','vanilla','blueberry','pineapple','kishmish']):
+        self.flavors = flavors
+        #used another attribute do not get confused by that. You can also use that:
+        #self.flavors = '\n -'.join(flavors)
+        self.flv = ',\n '.join(flavors)
+    
+    
+    def show_flavors(self):
+        """making method to show flavors
+        """
+        message = "This Restaurant has " + str(self.flv)
+        message += " flavors."
+        print(message)
+
+#callflavor = IceCreamStand()
+#callflavor.show_flavors()
