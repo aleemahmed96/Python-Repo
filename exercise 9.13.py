31d428e99244c2305fff80db0dca7bf03ab2531c
danidavid <dudeboy.aleemahmed@gmail.com>
2018-03-14 01:39:31 +0500

    for exercise 9.13

---
+++ exercise 9.13.py
@@ -0,0 +1,9 @@
+"""making an Import from privilieges_admin
+    which is Imported from user.py (Multiple Module)
+    see priviliges_admin.py and user.py for this exercise
+"""
+
+import privliges_admin
+
+admin1 = privliges_admin.Administrator()
+admin1.px.show_privileges()
---
+++ privliges_admin.py
@@ -0,0 +1,25 @@
+from user import User
+#import user
+# you can write etiher both of them
+
+class Privileges():
+    def __init__(self,privileges=["can add post","can delete post","can ban user"]):
+        self.priv = privileges
+
+    def show_privileges(self):
+            """used to show privileges
+            """
+            for self.p in self.priv:
+                msg = "\n The admin has privileges: \n"
+                msg += str(self.p)
+                print(msg)
+
+class Administrator(User):
+    """making a class for exercise 9.7 from exercise 9.5
+    """
+    def __init__(self):
+        self.px = Privileges()
+
+
+#admin = Administrator()
+#admin.px.show_privileges()
