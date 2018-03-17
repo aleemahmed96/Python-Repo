d3d2b1e29dbe33b5dec0a2e1cc2e135caf6385a0
danidavid <dudeboy.aleemahmed@gmail.com>
2018-03-17 17:47:13 +0500

    working...

---
+++ exercise 9.14.py
@@ -0,0 +1,32 @@
+from random import randint
+
+class Dice():
+    def __init__(self,sides):
+        self.side = sides
+        self.roll = 0
+        self.time = []
+
+
+    def roll_die(self):
+        """rolling dice 10 times
+        """
+        print("\nThe dice has sides: ",self.side)
+        count=0
+        for count in range(10):
+            self.roll = randint(1,self.side)
+            #print("The dice rolled now is:",self.roll)
+            self.time.append(self.roll)
+        print("The dice rolled 10 times gets: ", self.time)
+
+
+obj = Dice(6)
+obj.roll_die() 
+
+# for 10-sided dice
+obj = Dice(10)
+obj.roll_die() 
+
+# for 20-sided dice
+obj = Dice(20)
+obj.roll_die() 
+
