6bf67b1bb5e5b22f521510806f4cea9523e1d032
danidavid <dudeboy.aleemahmed@gmail.com>
2018-03-19 23:26:11 +0500

    Revert "working...."
    This reverts commit 194d9d505ccb071f3cebff75457c9fc75fea7819.

--- exercise 10.2.py
+++
@@ -1,9 +0,0 @@
-file_name = 'learning_python.txt'
-
-with open(file_name) as file:
-    lines = file.readlines()
-    print("\nfor storing lines in a list\n")
-
-for line in lines:
-    msg = line.replace("Python","C")
-    print(msg.strip())
