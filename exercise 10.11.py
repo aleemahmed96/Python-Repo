c104abc4998cf53ac0aca162d5eaee035c054560
danidavid <dudeboy.aleemahmed@gmail.com>
2018-03-24 21:05:03 +0500

    working....

--- exercise 10.11.py
+++ exercise 10.11.py
@@ -4,3 +4,8 @@ prompt = input("Enter your favourite number: ")
 filename = "fav_number.json"
 with open(filename,'w') as file:
     json.dump(prompt,file)
+
+with open(filename, mode='r') as file:
+    msg = json.load(file)
+    
+print("I know your favourite number. It's ",msg)
